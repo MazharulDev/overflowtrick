088b5d5876a1a1822910b1b8dd9c7509b71fd567
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-08-09 06:09:14 +0600

    protected home page route middleware and stable

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,40 +1,9 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect("/sign-in?callbackUrl=/");
-    },
-  });
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-
-      {session?.user ? (
-        <div>
-          <button className="bg-blue p-2 text-white" onClick={() => signOut()}>
-            Logout
-          </button>
-          <p className="text-white">{session?.user?.name}</p>
-        </div>
-      ) : (
-        <div>
-          <div
-            onClick={() =>
-              signIn("google", {
-                callbackUrl: "http://localhost:3000/",
-              })
-            }
-            className="flex justify-center items-center gap-4 bg-slate-800 hover:bg-slate-900 p-3 rounded-lg text-white mb-4 cursor-pointer"
-          >
-            <h2>Sign in with Google</h2>
-          </div>
-        </div>
-      )}
     </>
   );
 }
