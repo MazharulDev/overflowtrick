afa83b8d5af12532d758ffb9105db6ca16925ce9
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-08-24 22:52:57 +0600

    server link updated

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
 import { signIn, useSession } from "next-auth/react";
 import { useGetSingleUserQuery } from "@/redux/user/userApi";
 import { redirect } from "next/navigation";
@@ -51,10 +51,10 @@ const SignInPage = () => {
             <FcGoogle className="text-heading3-bold" />
             <button>Continue with Google</button>
           </div>
-          <div className="mt-3 flex justify-start items-center gap-3 p-3 border border-gray-300 hover:bg-dark-3 cursor-pointer rounded-lg duration-100">
+          {/* <div className="mt-3 flex justify-start items-center gap-3 p-3 border border-gray-300 hover:bg-dark-3 cursor-pointer rounded-lg duration-100">
             <FaGithub className="text-heading3-bold" />
             <button>Continue with Github</button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://overflowtrick-backend.vercel.app/api/v1",
   }),
   tagTypes: ["post", "like", "deletepost", "comment", "deletecomment"],
   endpoints: () => ({}),
