88e0eb301f8e9ae71e224532e0ad3e2905926009
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-11-28 22:39:47 +0600

    dashboard added exit button and some modify

--- src/app/components/dashboard/UI/DashboardTopbar.tsx
+++ src/app/components/dashboard/UI/DashboardTopbar.tsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 const DashboardTopbar = () => {
   return (
     <nav className="topbar">
-      <Link href="/dashboard" className="flex items-center  gap-4">
-        <h2 className="text-white text-heading2-bold font-bold">Dashboard</h2>
-        {/* <Link href="/" className="bg-white px-2 text-red-500 rounded-lg">
-          Exit
-        </Link> */}
-      </Link>
+      <div className="flex items-center  gap-4">
+        <Link href="/" className="bg-white px-2 text-red-500 rounded-lg">
+          <button>Exit</button>
+        </Link>
+        <Link href="/dashboard">
+          <h2 className="text-white text-heading2-bold font-bold">Dashboard</h2>
+        </Link>
+      </div>
       <div className="flex items-center gap-1"></div>
     </nav>
   );
---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h2 className="text-heading1-bold">Something went wrong!</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
